f04afc1feb2d6f4536ce58406c815e67e3d61a72
datatab <info@dejan.pro>
2022-10-25 22:55:49 +0200

    Change in base settings static dir.

--- config/settings/base.py
+++ config/settings/base.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(APPS_DIR / "static")]
+STATICFILES_DIRS = [str(ROOT_DIR / "static")]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
