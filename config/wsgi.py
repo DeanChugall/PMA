d2e537cdabe61f7e3c940832a033f4422d0d6023
datatab <info@dejan.pro>
2022-11-01 14:53:23 +0100

    DEV[#3]: Nastavak dev-a Landing Page.

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,6 +2,6 @@ import os
 from config.settings.base import env
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', env.str("DJANGO_SETTINGS_MODULE"))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', env.str("DJANGO_SETTINGS_MODULE", default="config.settings.local"))
 
 application = get_wsgi_application()
