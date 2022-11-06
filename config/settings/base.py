8c4a54378db6d0bb556185f9e119dea3ed48d781
datatab <info@dejan.pro>
2022-11-06 07:52:06 +0100

    DEV[#11]: Fix email join form with HTMX.
     - all ok.

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,6 +21,8 @@ SECRET_KEY = env.str("DJANGO_SECRET_KEY", default="Test")
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "squid-app-ktzit.ondigitalocean.app",
                  "pma-app-k89y6.ondigitalocean.app"]
 
+ADMIN_ENABLED = env.str("ADMIN_ENABLED", default="False")
+
 # APPS
 # ------------------------------------------------------------------------------
 DJANGO_APPS = [
