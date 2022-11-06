81d1bddb524eaec49a6a502bf130473f0a80835f
datatab <info@dejan.pro>
2022-11-06 08:04:32 +0100

    DEV[#11]: Fix email join form with HTMX.
     - all ok.

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.views import defaults as default_views
-from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
     path("", include("pma_apps.landing_page.urls", namespace="landing_page")),
 
-    path('admin/', admin.site.urls),
+
     path("users/", include("pma_apps.users.urls", namespace="users")),
 
     # path("servis/", include("pma_apps.auto_servis.urls", namespace="auto_servis")),
@@ -17,7 +18,10 @@ urlpatterns = [
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.ADMIN_ENABLED:
+    urlpatterns += [path('admin/', admin.site.urls),]
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
