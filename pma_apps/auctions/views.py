9b49b55aff4f5b2956a369cab20e9b4e8a400c10
datatab <info@dejan.pro>
2022-12-31 00:49:44 +0100

    DEV[#111]: Dodavanje Tag-a "GRAD" na svim listinzima.
    - Dodat TAG na listingu "SVI Aktivni Zahtevi".

--- pma_apps/auctions/views.py
+++ pma_apps/auctions/views.py
@@ -380,6 +380,8 @@ def aktivni_zahtevi_view(request):
 
     for auction in auctions:
         auction.image = auction.get_images.first()
+        auction.user = auction.creator.get_profil_vozaca
+
         if request.user in auction.watchers.all():
             auction.is_watched = True
         else:
