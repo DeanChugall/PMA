5b4fe11e760be057a649c95bc3ca9fcab9b1e4fa
datatab <info@dejan.pro>
2023-01-20 10:54:32 +0100

    FIX[#56]: Dodan  ```  'OPTIONS': {'sslmode': env.str("POSTGRES_SSLMODE",  default="require")},  ```  za PostGres konekciju.

--- config/settings/base.py
+++ config/settings/base.py
@@ -184,9 +184,7 @@ DATABASES = {
         "PASSWORD": env.str("POSTGRES_PASSWORD", default="pma_database"),
         "HOST": env.str("POSTGRES_HOST", default="0.0.0.0"),
         "PORT": env.str("POSTGRES_PORT", default="5432"),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        'OPTIONS': {'sslmode': env.str("POSTGRES_SSLMODE",  default="require")},
     }
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
